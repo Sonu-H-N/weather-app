1149f8a25928a42f957977e811f1883c21a69efe
Suhas-H-N <suhassuhas3335@gmail.com>
2026-02-24 09:58:07 +0530

    advanced weather UI

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
+const apiKey = "YOUR_API_KEY";
+
 async function getWeather() {
 
-    const city = document.getElementById("cityInput").value;
-    const apiKey = "YOUR_API_KEY";
+const city = document.getElementById("city").value;
 
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
 
-    try {
-        const response = await fetch(url);
-        const data = await response.json();
+const res = await fetch(url);
+const data = await res.json();
 
-        document.getElementById("cityName").innerText = data.name;
-        document.getElementById("temperature").innerText = data.main.temp + "°C";
-        document.getElementById("description").innerText = data.weather[0].description;
-        document.getElementById("humidity").innerText = data.main.humidity;
-        document.getElementById("wind").innerText = data.wind.speed;
+document.getElementById("temp").innerText = data.main.temp + "°C";
+document.getElementById("cityName").innerText = data.name;
+document.getElementById("desc").innerText = data.weather[0].description;
+document.getElementById("humidity").innerText = data.main.humidity;
+document.getElementById("wind").innerText = data.wind.speed;
 
-    } catch (error) {
-        alert("City not found!");
-    }
+const iconCode = data.weather[0].icon;
+document.getElementById("icon").src =
+`https://openweathermap.org/img/wn/${iconCode}@2x.png`;
 }
