cca76cefc3b3977493293bc7a01ed2e9dd72a823
Sonu-H-N <sonuhn0011@gmail.com>
2026-02-26 11:54:21 +0530

    Weather app project

--- script.js
+++ script.js
@@ -1,13 +1,47 @@
-function getWeather() {
+const apiKey = "YOUR_API_KEY";
 
-const city = document.getElementById("city").value;
+async function getWeather() {
 
-if (!city) {
-alert("Enter city name");
-return;
+    const city = document.getElementById("city").value;
+
+    if (!city) {
+        alert("Enter city name");
+        return;
+    }
+
+    const url =
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+
+    try {
+
+        const response = await fetch(url);
+        const data = await response.json();
+
+        document.getElementById("cityName").innerText =
+            data.name + ", " + data.sys.country;
+
+        document.getElementById("temp").innerText =
+            "🌡 Temperature: " + data.main.temp + " °C";
+
+        document.getElementById("desc").innerText =
+            "☁ " + data.weather[0].description;
+
+        document.getElementById("humidity").innerText =
+            "💧 Humidity: " + data.main.humidity + "%";
+
+        document.getElementById("wind").innerText =
+            "🌬 Wind: " + data.wind.speed + " m/s";
+
+        const icon =
+            `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+
+        document.getElementById("icon").src = icon;
+
+    } catch (error) {
+        alert("City not found");
+    }
 }
 
-document.getElementById("cityName").innerText = city;
-document.getElementById("temp").innerText = "30°C";
-
+function toggleTheme() {
+    document.body.classList.toggle("dark");
 }
