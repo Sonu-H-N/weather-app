b3ca109fd9446d72d80198ef5d493cf583d4113d
Sonu-H-N <sonuhn0011@gmail.com>
2026-03-09 18:19:19 +0530

    Added loading spinner for search

--- script.js
+++ script.js
@@ -30,3 +30,14 @@ alert("Voice recognition not supported in this browser");
 };
 
 }
+function showLoader(){
+
+document.getElementById("loader").style.display="block";
+
+}
+
+function hideLoader(){
+
+document.getElementById("loader").style.display="none";
+
+}
