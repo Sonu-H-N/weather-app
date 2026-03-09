b30c615abfdebc3d1069172485747418395a63b7
Sonu-H-N <sonuhn0011@gmail.com>
2026-03-09 18:13:39 +0530

    Added voice search feature

--- script.js
+++ script.js
@@ -5,3 +5,28 @@ const city=document.getElementById("cityInput").value;
 updateWeather(city);
 
 }
+function startVoiceSearch(){
+
+const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
+
+recognition.lang = "en-US";
+
+recognition.start();
+
+recognition.onresult = function(event){
+
+const city = event.results[0][0].transcript;
+
+document.getElementById("cityInput").value = city;
+
+updateWeather(city);
+
+};
+
+recognition.onerror = function(){
+
+alert("Voice recognition not supported in this browser");
+
+};
+
+}
